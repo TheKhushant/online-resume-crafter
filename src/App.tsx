814a4d8571e3a50265d48830d24445ed2f02c8b9
Khushant Wankhede <yashwankhede53@gmail.com>
2025-06-05 22:50:37 +0530

    disabled right click

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";   //to disable right click on website
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -5,32 +6,41 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import MouseTrail from "@/components/MouseTrail"; 
-
-// import ExperienceSection from "@/components/ExperienceSection";
-// import SmoothScrollProvider from "./components/SmoothScrollProvider";
+import MouseTrail from "@/components/MouseTrail";
 
 const queryClient = new QueryClient();
+//to disable right click on website
+const App = () => {
+  useEffect(() => {
+    const disableRightClick = (e) => e.preventDefault();
+    const disableSelect = (e) => e.preventDefault();
 
-const App = () => (
-    // <SmoothScrollProvider>
-    
+    document.addEventListener("contextmenu", disableRightClick);
+    document.addEventListener("selectstart", disableSelect);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+      document.removeEventListener("selectstart", disableSelect);
+    };
+  }, []);
+
+  return (
     <div data-scroll-container>
-        <MouseTrail />
-            <QueryClientProvider client={queryClient}>
-                <TooltipProvider>
-                    <Toaster />
-                    <Sonner />
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={<Index />} />
-                            <Route path="*" element={<NotFound />} />
-                        </Routes>
-                    </BrowserRouter>
-                </TooltipProvider>
-            </QueryClientProvider>
+      <MouseTrail />
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
     </div>
-    // </SmoothScrollProvider>
-);
+  );
+};
 
 export default App;
